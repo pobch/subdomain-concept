dd6725174dc9aaba5f10fbb732d835c9077831ae
Pob <590650@gmail.com>
2020-07-20 02:01:21 +0700

    server side

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(
+  cookieSession({
+    secret: 'secret-key',
+    sameSite: 'lax',
+  })
+)
+
+app.get('/', (req, res) => {
+  console.log(req.session.pob)
+  res.json({ message: 'Howdy, Pob' })
+})
+
+app.post('/postmsg', (req, res) => {
+  console.log(req.session.pob)
+  req.session.pob = Date.now()
+  res.json({ success: true })
+})
+
+app.listen(80, '127.0.0.5', () => {
+  console.log('Server started on 127.0.0.5:80')
+})
